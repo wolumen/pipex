47e1e58350409cb9b2bc5c619d58a7f4bf4e8987
Josefine Preissner <jpreissn@student.42wolfsburg.de>
2022-03-06 19:29:25 +0100

    Funktionen < 25 Zeilen ABER variabel lenght array

--- main.c
+++ main.c
@@ -12,8 +12,7 @@
 
 #include "pipex.h"
 
-
-void open_pipes(int pipe_fd[][2], int size)
+void	open_pipes(int pipe_fd[][2], int size)
 {
 	int	i;
 
@@ -25,7 +24,6 @@ void open_pipes(int pipe_fd[][2], int size)
 		i++;
 	}
 }
-	
 
 void	close_unused_pipes(int pipe_fd[][2], int size)
 {
@@ -36,6 +34,35 @@ void	close_unused_pipes(int pipe_fd[][2], int size)
 	{
 		close(pipe_fd[i][0]);
 		close(pipe_fd[i][1]);
+		i++;
+	}
+}
+
+void	process_forks(int pipe_fd[][2], int size, char *argv[], char *envp[])
+{
+	int	pids[size];
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		pids[i] = fork();
+		if (pids[i] == -1)
+			ft_error(errno, "fork");
+		if (pids[i] == 0)
+		{
+			if (i == 0)
+				dup2(pipe_fd[i][1], STDOUT_FILENO);
+			else if (i == size - 1)
+				dup2(pipe_fd[i - 1][0], STDIN_FILENO);
+			else
+			{
+				dup2(pipe_fd[i - 1][0], STDIN_FILENO);
+				dup2(pipe_fd[i][1], STDOUT_FILENO);
+			}
+			close_unused_pipes(pipe_fd, size - 1);
+			ft_exec(argv[i + 2], envp);
+		}
 		i++;
 	}
 }
@@ -55,89 +82,20 @@ void	change_std_io(char *infile, char *outfile)
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	int pids[COMMANDS];					// für jeden cmd ein child
-	int	pipe_fd[COMMANDS - 1][2];		// zwischen zwei cmd eine pipe
-	int i;
+	int	pipe_fd[COMMANDS - 1][2];
+	int	i;
 
 	if (argc < 5)
 	{
 		write(STDERR_FILENO, "ERROR ./pipex file1 \"cmd1\" \"cmd2\" file2\n", 67);
 		exit(EXIT_FAILURE);
 	}	
-
 	change_std_io(argv[1], argv[argc - 1]);
-
-	// pipes öffnen (eine weniger als cmds)
 	open_pipes(pipe_fd, COMMANDS - 1);
-
-	// processe erstellen (einen je cmd)
-	i = 0;
-	while (i < COMMANDS)
-	{
-		pids[i] = fork();
-		if (pids[i] == -1)
-			ft_error(errno, "fork");
-		if (pids[i] == 0)
-		{
-			// child [i] process
-			// break or ft_child_stuff();
-
-			if (i == 0)
-			{
-				// Child process 1
-				dup2(pipe_fd[i][1], STDOUT_FILENO);
-				// close all unused pipes in a loop
-				close_unused_pipes(pipe_fd, COMMANDS - 1);
-				ft_exec(argv[i + 2], envp);				
-			}
-			else if (i == COMMANDS - 1)
-			{
-				// Child process letzter
-				dup2(pipe_fd[i - 1][0], STDIN_FILENO);
-				// close all unused pipes in a loop
-				close_unused_pipes(pipe_fd, COMMANDS - 1);
-				ft_exec(argv[i + 2], envp);				
-			}
-			else
-			{
-				// Child processes between
-				dup2(pipe_fd[i - 1][0], STDIN_FILENO);
-				dup2(pipe_fd[i][1], STDOUT_FILENO);
-				// close all unused pipes in a loop
-				close_unused_pipes(pipe_fd, COMMANDS - 1);
-				ft_exec(argv[i + 2], envp);					
-			}
-		}
-		i++;
-	}
-
-
-	// close all unused pipes in a loop
+	process_forks(pipe_fd, COMMANDS, argv, envp);
 	close_unused_pipes(pipe_fd, COMMANDS - 1);
-
-	// wait for the children in a loop
-	i = 0;
-	while(i < COMMANDS)
-	{	
-		wait(NULL); 
-		// waitpid(pids[i], NULL, 0);
-		i++;
-	}
-
-
+	i = COMMANDS;
+	while (--i)
+		wait(NULL);
 	return (0);
 }
-
-
-
-// int open_pipes(int argc)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (i < COMMANDS -1)
-// 	{
-// 		if (pipe(pipe_fd[i]) == -1)
-// 			ft_error(errno);
-// 	}
-// }
